ea80e89593e7e7ac5191ac8c7967a2daeb0d5b50
CaroBuenrostro19 <70624310+CaroBuenrostro19@users.noreply.github.com>
2021-10-14 22:15:47 -0500

    Subí proyecto Unity Animaciones

---
+++ Assets/Assets/Códigos/Movimientos.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimientos : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent <Animator> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("d"))
+        {
+            anim.SetBool("Baile", true);
+        }
+        if (!Input.GetKey("d"))
+        {
+            anim.SetBool("Baile", false);
+        }
+        if (Input.GetKey("w"))
+        {
+            anim.SetBool("Caminado", true);
+        }
+        if (!Input.GetKey("w"))
+        {
+            anim.SetBool("Caminado", false);
+        }
+        if (Input.GetKey("r"))
+        {
+            anim.SetBool("Corriendo", true);
+        }
+        if (!Input.GetKey("r"))
+        {
+            anim.SetBool("Corriendo", false);
+        }
+        if (Input.GetKey("f"))
+        {
+            anim.SetBool("Caer", true);
+        }
+        if (!Input.GetKey("f"))
+        {
+            anim.SetBool("Caer", false);
+        }
+    }
+}
